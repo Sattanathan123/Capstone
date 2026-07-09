ffb14503b1cc2989fd6ca7f895a217090e1b08fd
Sattanathan@123 <vbsattanathan@gmail.com>
2026-07-09 22:16:00 +0530

    updated smtp

--- backend/src/main/java/com/dbi/backend/DbiBackendApplication.java
+++ backend/src/main/java/com/dbi/backend/DbiBackendApplication.java
@@ -1,10 +1,8 @@
 package com.dbi.backend;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
